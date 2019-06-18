ac91e4722ffd7359a754f08b833854853fa96e2d
Matthew Weaber <matthewweaber89@gmail.com>
2019-06-18 19:54:45 -0400

    Nav start

---
+++ client/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <div className="navbar">
+            <a className="newLink" href="/">
+                Carric Nailo Page
+            </a>
+        </div>
+    );
+}
+
+export default Nav;
